998cb0264456c5e85adcf3cdf240341dfd80071c
andre.li <434795063@qq.com>
2026-06-02 20:38:40 +0800

    feat(web): replace neon glow theme with Linear Dark theme

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router";
-import "./styles/glow.css";
+import "./styles/theme.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
